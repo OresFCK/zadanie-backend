51e463e239194ec23220d0c24c240ff5a439d95d
Adrian <adrian@cosmicweb.io>
2024-07-15 11:27:36 +0200

    finished generate pdf logic

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -21,7 +21,6 @@ class FormController extends AbstractController
         $person->setFirstName($data['firstName']);
         $person->setLastName($data['lastName']);
         $person->setAdress($data['address']);
-        $person->setPhoneNumber($data['phoneNumber']);
         
         $em->persist($person);
         $em->flush();
@@ -41,5 +40,41 @@ class FormController extends AbstractController
         $em->flush();
         
         return new Response('Text entry created', 201);
+    }
+
+    #[Route('/api/text-entries', methods: ['GET'])]
+    public function getTextEntries(EntityManagerInterface $em): Response
+    {
+        $repository = $em->getRepository(TextEntry::class);
+        $textEntries = $repository->findAll();
+
+        $data = [];
+        foreach ($textEntries as $textEntry) {
+            $data[] = [
+                'id' => $textEntry->getId(),
+                'content' => $textEntry->getContent(),
+            ];
+        }
+
+        return $this->json($data);
+    }
+
+    #[Route('/api/personal-infos', methods: ['GET'])]
+    public function getPersonalInfos(EntityManagerInterface $em): Response
+    {
+        $repository = $em->getRepository(PersonalInfo::class);
+        $personalInfos = $repository->findAll();
+
+        $data = [];
+        foreach ($personalInfos as $personalInfo) {
+            $data[] = [
+                'id' => $personalInfo->getId(),
+                'firstName' => $personalInfo->getFirstName(),
+                'lastName' => $personalInfo->getLastName(),
+                'address' => $personalInfo->getAdress(),
+            ];
+        }
+
+        return $this->json($data);
     }
 }
--- src/Controller/PdfController.php
+++ src/Controller/PdfController.php
@@ -2,41 +2,62 @@
 
 namespace App\Controller;
 
-use App\Entity\Person;
-use App\Entity\PersonalInfo;
-use App\Entity\TextEntry;
 use Dompdf\Dompdf;
 use Dompdf\Options;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\PersonalInfo;
+use App\Entity\TextEntry;
+use Twig\Environment;
 
 class PdfController extends AbstractController
 {
-    /**
-     * @Route("/generate-pdf", name="generate_pdf", methods={"POST"})
-     */
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    #[Route('/api/generate-pdf', methods: ['POST', 'HEAD'])]
     public function generatePdf(Request $request, EntityManagerInterface $em): Response
     {
         $data = json_decode($request->getContent(), true);
+
+        if (!isset($data['personId']) || !isset($data['textEntryId'])) {
+            return new Response('Invalid request data', 400);
+        }
+
         $person = $em->getRepository(PersonalInfo::class)->find($data['personId']);
         $textEntry = $em->getRepository(TextEntry::class)->find($data['textEntryId']);
-        
-        $content = sprintf(
-            "Imię: %s\nNazwisko: %s\nAdres: %s\n\n%s",
-            $person->getFirstName(),
-            $person->getLastName(),
-            $person->getAdress(),
-            $textEntry->getContent()
-        );
 
-        // Generate PDF
+        if (!$person || !$textEntry) {
+            return new Response('Person or Text Entry not found', 404);
+        }
+
+        $htmlContent = $this->twig->render('./pdf/generate_pdf.html.twig', [
+            'person' => [
+                'firstName' => $person->getFirstName(),
+                'lastName' => $person->getLastName(),
+                'address' => $person->getAdress(),
+            ],
+            'textEntry' => [
+                'content' => $textEntry->getContent(),
+            ],
+        ]);
+
+        $utf8Content = $this->ensureUtf8Encoding($htmlContent);
+
         $options = new Options();
         $options->set('defaultFont', 'Arial');
+        $options->set('isHtml5ParserEnabled', true); 
+        $options->set('isPhpEnabled', true); 
+
         $dompdf = new Dompdf($options);
-        $dompdf->loadHtml($content);
+        $dompdf->loadHtml($utf8Content); 
         $dompdf->setPaper('A4', 'portrait');
         $dompdf->render();
         
@@ -46,5 +67,25 @@ class PdfController extends AbstractController
             'Content-Type' => 'application/pdf',
             'Content-Disposition' => 'inline; filename="document.pdf"'
         ]);
+    }
+
+    /**
+     * Ensure content is UTF-8 encoded
+     * @param string $content
+     * @return string
+     */
+    private function ensureUtf8Encoding(string $content): string
+    {
+        if (mb_check_encoding($content, 'UTF-8')) {
+            return $content;
+        }
+
+        $encoding = mb_detect_encoding($content, 'UTF-8, ISO-8859-1', true);
+
+        if ($encoding !== 'UTF-8') {
+            $content = mb_convert_encoding($content, 'UTF-8', $encoding);
+        }
+
+        return $content;
     }
 }
